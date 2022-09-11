1c9149466c4f3f01454f5b161001a6fc10c49f5c
Vincent Bernet <vincent.bernet99@gmail.com>
2022-09-11 23:25:20 +0200

    feature: finishing the table

--- front-end/src/components/MainCardContainer.tsx
+++ front-end/src/components/MainCardContainer.tsx
@@ -12,7 +12,9 @@ const MainCardContainer = ({TeamMembers} : MainCardContainerProps) => {
     return (    
     <div className="MainCardContainer">
         <TimerCard timer={"2 H 35 Min 45 Secondes"}/>
-        {TeamMembers.map(name=> (<MemberCard memberInfo={name}/>))}
+        <div className="MemberCardContainer">
+            {TeamMembers.map(name=> (<MemberCard memberInfo={name} key={"MemberCard of " + name}/>))}
+        </div>
     </div>);
 };
 
--- front-end/src/components/MemberCard.tsx
+++ front-end/src/components/MemberCard.tsx
@@ -5,8 +5,8 @@ interface MemberCardProp {
 
 const MemberCard = ({memberInfo }: MemberCardProp) => {
     return (
-    <div key={memberInfo} className="MemberCard">
-        <span>{memberInfo}</span>
+    <div key={memberInfo + " container"} className="MemberCard">
+        <span key={memberInfo + " name"}>{memberInfo}</span>
     </div>);
 }
 
