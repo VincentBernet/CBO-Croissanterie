e22496969b9574ec9c30774d804bb171345531a4
VincentBernet <vincent.bernet@efrei.net>
2022-11-01 00:10:53 +0100

    feature: returning following date format hh:mm:ss

--- back-end/src/members-list/services/members-list-mock.ts
+++ back-end/src/members-list/services/members-list-mock.ts
@@ -7,7 +7,7 @@ const MockCurrentTeamMembers: memberListType = [{ name: 'Vincent' },
 { name: 'Kevin' }, { name: 'Jean' }, { name: 'Jean-Christophe' }, { name: 'Hakima' }, { name: 'Aya' }, { name: 'Virgil' }, { name: 'Stéphane' }, { name: 'Wendy' }, { name: 'Oum' }, { name: 'Jeremy' }, { name: 'Claudia' }];
 
 const MockBeginingTime: number = 0;
-const MockEndingTime: number = 17 * 60;
+const MockEndingTime: number = 18 * 60;
 const MockCurrentDate: Date = new Date;
 const MockCurrentDateInMinute: number = (MockCurrentDate.getHours() * 60) + MockCurrentDate.getMinutes() + (MockCurrentDate.getSeconds() / 60);
 
