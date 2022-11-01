8341786a3368e5c0ffc6c8968b27585611489a6a
VincentBernet <vincent.bernet@efrei.net>
2022-11-01 19:15:18 +0100

    technical: modifying end game value for testing purpose

--- back-end/src/members-list/services/members-list-mock.ts
+++ back-end/src/members-list/services/members-list-mock.ts
@@ -7,7 +7,7 @@ const MockCurrentTeamMembers: memberListType = [{ name: 'Vincent' },
 { name: 'Kevin' }, { name: 'Jean' }, { name: 'Jean-Christophe' }, { name: 'Hakima' }, { name: 'Aya' }, { name: 'Virgil' }, { name: 'Stéphane' }, { name: 'Wendy' }, { name: 'Oum' }, { name: 'Jeremy' }, { name: 'Claudia' }];
 
 const MockBeginingTimer: number = 0;
-const MockEndingTimer: number = 17 * 60;
+const MockEndingTimer: number = 24 * 60;
 const MockCurrentDate: Date = new Date;
 const MockCurrentDateInMinute: number = (MockCurrentDate.getHours() * 60) + MockCurrentDate.getMinutes() + (MockCurrentDate.getSeconds() / 60);
 
