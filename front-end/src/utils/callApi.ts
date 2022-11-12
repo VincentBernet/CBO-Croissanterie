114961203822b6aa7028df7aa2a4c88e21126157
VincentBernet <vincent.bernet@efrei.net>
2022-11-12 16:56:50 +0100

    feature: correction bug date v2

--- front-end/src/utils/callApi.ts
+++ front-end/src/utils/callApi.ts
@@ -5,6 +5,7 @@ export const retrieveApiCall = async (): Promise<dtoMembersListAPI> => {
     let json: dtoMembersListAPI = { timerBeforeNextDeletion: false, memberList: [{ name: "" }], beginingTimer: { hours: 8, minutes: 0, seconds: 0 }, endingTimer: { hours: 17, minutes: 0, seconds: 0 } };
     const currentDateApiCall: Date = new Date();
     const currentDateApiCallInMinute: number = (currentDateApiCall.getHours() * 60) + currentDateApiCall.getMinutes() + (currentDateApiCall.getSeconds() / 60);
+    console.log("currentDateApiCallInMinute = " + currentDateApiCallInMinute);
     try {
         const endpointUrl = `https://own-proxy-cors.herokuapp.com/https://croissanterie-backend.herokuapp.com/members/${currentDateApiCallInMinute}`;
         await fetch(endpointUrl).then((response) => response
